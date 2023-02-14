655c22a43a84fd2c39d4f895bbaf016e215aace1
talhaustundag0 <124690462+talhaustundag0@users.noreply.github.com>
2023-02-14 09:49:59 +0300

    Add files via upload
    aa

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -4,3 +4,12 @@ var swiper = new Swiper(".swiper1",{
         dynamicBullets:true,
     },
 });
+
+var swiper2 = new Swiper(".swiper2",{
+    slidesPerView:3,
+    spaceBetween:10,
+    pagination:{
+        el:".swiper-pagination",
+        clickable:true,
+    },
+})
